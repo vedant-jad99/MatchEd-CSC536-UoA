9e55a699f95d53f40c4630c1b8dbc81c6e3b7e52
atom-cunningham <laser@email.arizona.edu>
2025-04-07 19:41:07 -0700

    dont auto update db

--- backend/main.go
+++ backend/main.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"os"
 
-	"backend/controllers"
 	"backend/models"
 	"backend/routes"
 
@@ -25,7 +24,7 @@ func main() {
 	router := routes.SetupRouter()
 
 	// initialize the tables
-	controllers.InitializeTables(models.DB)
+	//controllers.InitializeTables(models.DB)
 
 	// Get port from environment variable or default to 3000
 	port := os.Getenv("PORT")
